99266ca5d4389e141f6d2c4c2d6bc99d9c7c0487
Navoddakshine <96253618+Navoddakshine@users.noreply.github.com>
2024-03-13 13:40:19 +0100

    Update line.py

--- .Sensors/line.py
+++ .Sensors/line.py
@@ -1,26 +1,23 @@
-int Sensor  =  10;  // Declaration of the sensor input spin
-   
-void setup  ( )
-{
-  Serial.begin (9600)  ;  // Initialization  serial output
-  pinMode (Sensor, INPUT)   ;  //  Initialization Sensorpin
-}
-  
-//  The program reads the current state of the sensor pin and
-// displays in the serial console whether the line tracker is currently 
-// on the line or not
-void loop  ( )
-{
-  bool val =  digitalRead (sensor)   ;  //  The current signal on the sensor is read
-  
-  if   (val  = =  HIGH )  //  If a signal has been detected, the LED is switched on.
-  {
-    Serial.println (“LineTracker is above the line”);
-  }
-  else
-  {
-    Serial.println (“Linetracker is out of line”);
-  }
-  Serial.println (“------------------------------------”)  ;
-  delay (500)  ;  // Pasuse between the measurement of 500ms
-}
+from machine import Pin
+import time
+
+#CHANGE PIN
+SENSOR_PIN = 7
+
+line_sensor = Pin(SENSOR_PIN, Pin.IN)
+
+def setup():
+    print("Line sensor setup complete")
+
+def loop():
+    val = line_sensor.value()
+    if val == 1:
+        print("Line detected")
+    else:
+        print("Line NOT detected")
+    time.sleep_ms(500)
+
+setup()
+
+while True:
+    loop()
