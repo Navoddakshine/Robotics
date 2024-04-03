ada05a79dc60f51ce6ac6ab23cc0ff73e2a81988
Dylan Nibourg <d.nibourg@st.hanze.nl>
2024-04-03 09:53:47 +0200

    PS4 Controller updated

--- Drive Controller/PS4_Controller/PS4_Controller.ino
+++ Drive Controller/PS4_Controller/PS4_Controller.ino
@@ -5,6 +5,8 @@ TEST
 https://techtutorialsx.com/2018/03/09/esp32-arduino-getting-the-bluetooth-device-address/
 https://github.com/pablomarquez76/PS4_Controller_Host/tree/main/examples
 
+https://github.com/jkb-git/ESP32Servo/blob/master/examples/Multiple-Servo-Example-Arduino/Multiple-Servo-Example-Arduino.ino
+
 MAC-Address: 30:AE:A4:99:FF:36
 
 */
@@ -22,6 +24,7 @@ MAC-Address: 30:AE:A4:99:FF:36
 //#include "esp_bt_main.h"
 //#include "esp_bt_device.h"
 #include <PS4Controller.h>
+#include<debounce.h>
 //#include <analogWrite.h>
 #include <ESP32Servo.h>
 
@@ -32,12 +35,19 @@ MAC-Address: 30:AE:A4:99:FF:36
 ## |___/\___|_.__/\_,_\__, |  ##
 ##                    |___/  ##
 ############################################################ DEBUG MODES */
-#define DEBUG_sticks 1
-#define DEBUG_triggers 1
-#define DEBUG_bumpers 0
-#define DEBUG_ESC 1
-#define DEBUG_Servo 1
-#define DEBUG_Offset 0
+
+
+#define DEBUG_Servo 0
+#define DEBUG_ARM 0
+
+#define DEBUG_Trim 0
+#define REPORT_ESC 1
+#define REPORT_Offset 1
+
+#define REPORT_Sticks 0
+#define REPORT_triggers 1
+
+
 
 int period = 1000;
 unsigned long time_now = 0;
@@ -56,18 +66,21 @@ const int ESC_L_PIN = 17;
 const int ESC_R_PIN = 16;
 
 //const int Servo_PIN [4] = {32, 33, 25, 26}; // x_L, y_L, x_R, y_R
-const int Servo_x_L_PIN = 33;
-const int Servo_y_L_PIN = 32;
-const int Servo_x_R_PIN = 26;
-const int Servo_y_R_PIN = 25;
+const int Servo_x_L_PIN = 32;
+const int Servo_y_L_PIN = 33;
+const int Servo_x_R_PIN = 25;
+const int Servo_y_R_PIN = 26;
 
 
-/*#########################################
+/*########################################
 ##  ___      _ _    __   __            ##
 ## |_ _|_ _ (_) |_  \ \ / /_ _ _ _    ##
 ##  | || ' \| |  _|  \ V / _` | '_|  ##
 ## |___|_||_|_|\__|   \_/\__,_|_|   ##
 ################################################## Initialize variables */
+
+bool ARMED = false;
+bool ARMED_prev = false;
 int speed = 0;
 int v_x = 0;
 int v_y = 0;
@@ -85,15 +98,28 @@ int loopCount = 0;
 /*#################
 ## Drive System ##
 ########################################################################*/
+
+// Gimbals --------------------------------------------------------------
+// hold the L1 and/or R1 button and the D-Pad to adjust the trim for the x and y directions.
+int Gimbal_L_Offset[2] = {0, 0};  // y, x
+int Gimbal_R_Offset[2] = {0, 0};  // y, x
+
+int Trim_range[2] = {-500, 500};  // min, max
+
+
 // ESCs ------------------------------------------------------------------
 Servo ESC_L;
 Servo ESC_R;
 
 const int ESC_min = 1000;
-const int ESC_max = 1200;
+const int ESC_set = 1500;
+const int ESC_max = 1800;
 
-int ESC_L_output = ESC_min;
-int ESC_R_output = ESC_min;
+
+
+int ESC_output = ESC_min;
+//int ESC_L_output = ESC_min;
+//int ESC_R_output = ESC_min;
 
 // Servos ----------------------------------------------------------------
 Servo servo_x_L;
@@ -101,16 +127,9 @@ Servo servo_y_L;
 Servo servo_x_R;
 Servo servo_y_R;
 
-const int Servo_min = 1300;
+const int Servo_min = 1250;
 const int Servo_mid = 1500;
-const int Servo_max = 1700;
-
-//int Servo_Offset [4] = {0, 0, 0, 0};
-int Servo_Offset_x_L = 0;
-int Servo_Offset_y_L = 0;
-int Servo_Offset_x_R = 0;
-int Servo_Offset_y_R = 0;
-
+const int Servo_max = 1750;
 
 int Servo_x_L_output = Servo_mid;
 int Servo_y_L_output = Servo_mid;
@@ -123,11 +142,11 @@ ESP32PWM pwm;
 /*###################
 ## PS4 Controller ##
 ########################################################################*/
+unsigned long debounceDelay = 50;    // debounce time
 // LED -------------------------------------------------------------------
 int r = 255;
 int g = 0;
 int b = 0;
-
 
 // Inputs ----------------------------------------------------------------
 
@@ -147,17 +166,14 @@ int R2input = 0;
 int L2range [2] = {0, 255};
 int R2range [2] = {0, 255};
 
-// Bumpers
-bool L1input = false;
-bool R1input = false;
-
-// D-Pad
-bool Upinput = false;
-bool Downinput = false;
-bool Leftinput = false;
-bool Rightinput = false;
 
 
+
+
+
+/*##############
+## Bluetooth ##
+########################################################################*/
 /*
 bool initBluetooth() {
   if (!btStart()) {
@@ -204,169 +220,127 @@ void nextRainbowColor() {
   if (b > 0 && g == 0) { r++; b--; }
 }
 
-void PS4_sendData() {
-    // Sets the color of the controller's front light
-    // Params: Red, Green,and Blue
-    // See here for details: https://www.w3schools.com/colors/colors_rgb.asp
-    PS4.setLed(r, g, b);
-    nextRainbowColor();
 
-    // Sets how fast the controller's front light flashes
-    // Params: How long the light is on , how long the light is off
-    // Range: 0->255 (255 = 2550ms), Set to 0, 0 for the light to remain on
-    PS4.setFlashRate(PS4.LStickY(), PS4.RStickY());
 
-    // Sets the rumble of the controllers
-    // Params: Weak rumble intensity, Strong rumble intensity
-    // Range: 0->255
-    PS4.setRumble(PS4.L2Value(), PS4.R2Value());
-
-    // Sends data set in the above three instructions to the controller
-    PS4.sendToController();
-
-    // Don't send data to the controller immediately, will cause buffer overflow
-    delay(10);
-}
-
-void PS4_recieveData() {
-  // Below has all accessible outputs from the controller
-    if (PS4.Right()) Serial.println("Right Button");
-    if (PS4.Down()) Serial.println("Down Button");
-    if (PS4.Up()) Serial.println("Up Button");
-    if (PS4.Left()) Serial.println("Left Button");
-
-    if (PS4.Square()) Serial.println("Square Button");
-    if (PS4.Cross()) Serial.println("Cross Button");
-    if (PS4.Circle()) Serial.println("Circle Button");
-    if (PS4.Triangle()) Serial.println("Triangle Button");
-
-    if (PS4.UpRight()) Serial.println("Up Right");
-    if (PS4.DownRight()) Serial.println("Down Right");
-    if (PS4.UpLeft()) Serial.println("Up Left");
-    if (PS4.DownLeft()) Serial.println("Down Left");
-
-    if (PS4.L1()) Serial.println("L1 Button");
-    if (PS4.R1()) Serial.println("R1 Button");
-
-    if (PS4.Share()) Serial.println("Share Button");
-    if (PS4.Options()) Serial.println("Options Button");
-    if (PS4.L3()) Serial.println("L3 Button");
-    if (PS4.R3()) Serial.println("R3 Button");
-
-    if (PS4.PSButton()) Serial.println("PS Button");
-    if (PS4.Touchpad()) Serial.println("Touch Pad Button");
-
-    if (PS4.L2()) {
-      Serial.printf("L2 button at %d\n", PS4.L2Value());
+// Buttons
+//https://github.com/kimballa/button-debounce/blob/main/examples/pressAndHold/pressAndHold.ino
+static void ARM_buttonHandler(uint8_t btnId, uint8_t btnState) {
+  if (btnState == BTN_PRESSED) {
+    switch(btnId) {
+      case 4:
+        ARMED = true;
+        PS4.setLed(255, 0, 0);
+        PS4.sendToController();
+        break;
+      case 5:
+        ARMED = false;
+        PS4.setLed(0, 255, 0);
+        PS4.sendToController();
+        break;
     }
-    if (PS4.R2()) {
-      Serial.printf("R2 button at %d\n", PS4.R2Value());
-    }
-
-    if (PS4.LStickX()) {
-      Serial.printf("Left Stick x at %d\n", PS4.LStickX());
-    }
-    if (PS4.LStickY()) {
-      Serial.printf("Left Stick y at %d\n", PS4.LStickY());
-    }
-    if (PS4.RStickX()) {
-      Serial.printf("Right Stick x at %d\n", PS4.RStickX());
-    }
-    if (PS4.RStickY()) {
-      Serial.printf("Right Stick y at %d\n", PS4.RStickY());
-    }
-
-    if (PS4.Charging()) Serial.println("The controller is charging");
-    if (PS4.Audio()) Serial.println("The controller has headphones attached");
-    if (PS4.Mic()) Serial.println("The controller has a mic attached");
-
-    Serial.printf("Battery Level : %d\n", PS4.Battery());
-    Serial.println();
-    delay(1000);
+  } else {
+    // btnState == BTN_OPEN.
+    if (DEBUG_ARM) Serial.println("Released button");
+  }
 }
 
 
-void PS4_getControlInput(){
-    //PS4_sendData();
-    //PS4_recieveData();
-
-    LSXinput = PS4.LStickX();
-    LSYinput = PS4.LStickY();
-    RSXinput = PS4.RStickX();
-    RSYinput = PS4.RStickY();
-
-    L2input = PS4.L2Value();
-    R2input = PS4.R2Value();
+static void Trim_buttonHandler(uint8_t btnId, uint8_t btnState) {
+  int us_step = 10;
+  if (btnState == BTN_PRESSED) {
+    switch(btnId) {
+      case 0:
+        if (PS4.L1() && Gimbal_L_Offset[0] < Trim_range[1]) {
+          Gimbal_L_Offset[0] += us_step;
+        }
+        if (PS4.R1() && Gimbal_R_Offset[0] < Trim_range[1]) {
+          Gimbal_R_Offset[0] += us_step;
+        }
+        break;
+      case 1:
+        if (PS4.L1() && Gimbal_L_Offset[0] > Trim_range[0]) {
+          Gimbal_L_Offset[0] -= us_step;
+        }
+        if (PS4.R1() && Gimbal_R_Offset[0] > Trim_range[0]) {
+          Gimbal_R_Offset[0] -= us_step;
+        }
+        break;
+      case 2:
+        if (PS4.L1() && Gimbal_L_Offset[1] > Trim_range[0]) {
+          Gimbal_L_Offset[1] -= us_step;
+        }
+        if (PS4.R1() && Gimbal_R_Offset[1] > Trim_range[0]) {
+          Gimbal_R_Offset[1] -= us_step;
+        }
+        break;
+      case 3:
+        if (PS4.L1() && Gimbal_L_Offset[1] < Trim_range[1]) {
+          Gimbal_L_Offset[1] += 10;
+        }
+        if (PS4.R1() && Gimbal_R_Offset[1] < Trim_range[1]) {
+          Gimbal_R_Offset[1] += 10;
+        }
+        break;
+    }
+    if (DEBUG_Trim) Serial.println("Trim Adjusted");
+  } else {
+    // btnState == BTN_OPEN.
+    if (DEBUG_Trim) Serial.println("Released button");
+  }
 }
+// Define your button with a unique id (0) and handler function.
+// (The ids are so one handler function can tell different buttons apart if necessary.)
+static Button UpButton(0, Trim_buttonHandler);
+static Button DownButton(1, Trim_buttonHandler);
+static Button LeftButton(2, Trim_buttonHandler);
+static Button RightButton(3, Trim_buttonHandler);
+
+static Button L3Button(4, ARM_buttonHandler);
+static Button R3Button(5, ARM_buttonHandler);
+
+static void pollButtons() {
+  // update() will call buttonHandler() if PIN transitions to a new state and stays there
+  // for multiple reads over 25+ ms.
+  UpButton.update(PS4.Up());
+  DownButton.update(PS4.Down());
+  LeftButton.update(PS4.Left());
+  RightButton.update(PS4.Right());
+  L3Button.update(PS4.L3());
+  R3Button.update(PS4.R3());
+}
+
+static void pollAnalog(){
+  LSXinput = PS4.LStickX();
+  LSYinput = PS4.LStickY();
+  RSXinput = PS4.RStickX();
+  RSYinput = PS4.RStickY();
+
+  L2input = PS4.L2Value();
+  R2input = PS4.R2Value();
+}
+
+
+
 
 /*#################
 ## Drive System ##
 ########################################################################*/
-void setOffset(){
-  if (PS4.L1()) {
-      //Serial.println("L1");
-      if (PS4.UpRight()) {
-        Serial.println("UpRight");
-        Servo_Offset_x_L = Servo_Offset_x_L + 10;
-      }
-      if (PS4.DownRight()) {
-        Serial.println("DownRight");
-        Servo_Offset_x_L = Servo_Offset_x_L - 10;
-      }
-      if (PS4.UpLeft()) {
-        Serial.println("UpLeft");
-        Servo_Offset_y_L = Servo_Offset_y_L + 10;
-      }
-      if (PS4.DownLeft()) {
-        Serial.println("DownLeft");
-        Servo_Offset_y_L = Servo_Offset_y_L - 10;
-      }
-    }
-
-
-    if (PS4.R1()) {
-      //Serial.println("R1");
-      if (PS4.UpRight()) {
-        Serial.println("UpRight");
-        Servo_Offset_x_R = Servo_Offset_x_R + 10;
-      }
-      if (PS4.DownRight()) {
-        Serial.println("DownRight");
-        Servo_Offset_x_R = Servo_Offset_x_R - 10;
-      }
-      if (PS4.UpLeft()) {
-        Serial.println("UpLeft");
-        Servo_Offset_y_R = Servo_Offset_y_R + 10;
-      }
-      if (PS4.DownLeft()) {
-        Serial.println("DownLeft");
-        Servo_Offset_y_R = Servo_Offset_y_R - 10;
-      }
-    }
-}
-
-void setECS_us(int id, int us) {
-  us = constrain(us, ESC_min, ESC_max);
-  switch (id) {
-    case 0:
-      ESC_L.write(us);
-      break;
-    case 1:
-      ESC_R.write(us);
-      break;
-  }
-}
 
 void setSpeed() {
   if (PS4.R2()) {
-      speed = speed + PS4.R2Value();
-    }
+    speed = map(PS4.R2Value(), R2range[0], R2range[1], ESC_min, ESC_max);
+    speed = constrain(speed, ESC_min, ESC_max);
+    if (ESC_output < speed) ESC_output = speed;
+  }
   if (PS4.L2()) {
-      speed = speed - PS4.L2Value();
-    }  
-  setECS_us(0, speed);
-  setECS_us(1, speed);
+    speed = map(PS4.L2Value(), L2range[0], L2range[1], ESC_max, ESC_min);
+    speed = constrain(speed, ESC_min, ESC_max);
+    if (ESC_output > speed) ESC_output = speed;
+  }
+  ESC_L.write(ESC_output);
+  ESC_R.write(ESC_output);
 }
+
 
 void setServo_us(int id, int us) {
   us = constrain(us, Servo_min, Servo_max);
@@ -390,79 +364,98 @@ void setVector() {
 
   v_x = RSXinput;
   v_y = RSYinput;
+  omega = LSXinput;
 
   Servo_x_L_output = map(v_x, RSXrange[0], RSXrange[1], Servo_min, Servo_max);
   Servo_y_L_output = map(-v_y, RSYrange[0], RSYrange[1], Servo_min, Servo_max);
   Servo_x_R_output = map(v_x, RSXrange[0], RSXrange[1], Servo_min, Servo_max);
   Servo_y_R_output = map(v_y, RSYrange[0], RSYrange[1], Servo_min, Servo_max);
 
-  setServo_us(0, Servo_x_L_output + Servo_Offset_x_L);
-  setServo_us(1, Servo_y_L_output + Servo_Offset_y_L);
-  setServo_us(2, Servo_x_R_output + Servo_Offset_x_R);
-  setServo_us(3, Servo_y_R_output + Servo_Offset_y_R);
+  setServo_us(0, Servo_x_L_output + Gimbal_L_Offset[1]);
+  setServo_us(1, Servo_y_L_output + Gimbal_L_Offset[0]);
+  setServo_us(2, Servo_x_R_output + Gimbal_R_Offset[1]);
+  setServo_us(3, Servo_y_R_output + Gimbal_R_Offset[0]);
 
 }
 
-/*##########
-## DEBUG ##
+/*###################
+## DEBUG & REPORT ##
 ########################################################################*/
-void DEBUGreport(){
+void DEBUG_REPORT(){
   if(millis() >= time_now + period){
     time_now += period;
-    
-    if (DEBUG_sticks) {
-      Serial.print("LSX: ");
-      Serial.println(LSXinput);
-      Serial.print("LSY: ");
-      Serial.println(LSYinput);
-      Serial.print("RSX: ");
-      Serial.println(RSXinput);
-      Serial.print("RSY: ");
-      Serial.println(RSYinput);
-    }
-    if (DEBUG_triggers) {
-      Serial.print("L2: ");
-      Serial.println(L2input);
-      Serial.print("R2: ");
-      Serial.println(R2input);
-    }
-    if (DEBUG_bumpers) {
-      Serial.print("L1: ");
-      Serial.println(PS4.L1());
-      Serial.print("R1: ");
-      Serial.println(PS4.R1());
-    }
 
-    if (DEBUG_ESC) {
-      Serial.print("ESC_L: ");
-      Serial.println(ESC_L_output);
-      Serial.print("ESC_R: ");
-      Serial.println(ESC_R_output);
-    }
-    if (DEBUG_Servo) {
-      Serial.print("Servo_x_L: ");
-      Serial.println(Servo_x_L_output);
-      Serial.print("Servo_y_L: ");
-      Serial.println(Servo_y_L_output);
-      Serial.print("Servo_x_R: ");
-      Serial.println(Servo_x_R_output);
-      Serial.print("Servo_y_R: ");
-      Serial.println(Servo_y_R_output);
-    }
+    //Serial.print("BT MAC Address: ");
+    //printDeviceAddress();
+    //Serial.println();
+    Serial.print("===================== ");
 
-    if (DEBUG_Offset) {
-      Serial.print("Servo_Offset_x_L: ");
-      Serial.println(Servo_Offset_x_L);
-      Serial.print("Servo_Offset_y_L: ");
-      Serial.println(Servo_Offset_y_L);
-      Serial.print("Servo_Offset_x_R: ");
-      Serial.println(Servo_Offset_x_R);
-      Serial.print("Servo_Offset_y_R: ");
-      Serial.println(Servo_Offset_y_R);
-    } 
+    if (PS4.isConnected()) {
+      Serial.println("Connected!");
+      // Active Buttons -----------------------------------------------------
+      if (PS4.L1()) Serial.println("L1 Button");
+      if (PS4.R1()) Serial.println("R1 Button");
+      if (PS4.L3()) Serial.println("L3 Button");
+      if (PS4.R3()) Serial.println("R3 Button");
 
-    if (PS4.Charging()) Serial.println("The controller is charging");
-    Serial.printf("Controller Battery Level : %d\n", PS4.Battery());
+      if (PS4.Right()) Serial.println("Right Button");
+      if (PS4.Down()) Serial.println("Down Button");
+      if (PS4.Up()) Serial.println("Up Button");
+      if (PS4.Left()) Serial.println("Left Button");
+
+      Serial.println("---------------------");
+       // Variables ----------------------------------------------------------
+      if (REPORT_Offset) {
+        Serial.print("Offset: ");
+        Serial.print("Left: ");
+        Serial.print(Gimbal_L_Offset[0]);
+        Serial.print(", ");
+        Serial.print(Gimbal_L_Offset[1]);
+        Serial.println("  // y, x");
+
+        Serial.print("Right: ");
+        Serial.print(Gimbal_R_Offset[0]);
+        Serial.print(", ");
+        Serial.print(Gimbal_R_Offset[1]);
+        Serial.println("  // y, x");
+      } 
+      Serial.println("---------------------");
+      if (REPORT_Sticks) {
+        Serial.print("LSX: ");
+        Serial.println(LSXinput);
+        Serial.print("LSY: ");
+        Serial.println(LSYinput);
+        Serial.print("RSX: ");
+        Serial.println(RSXinput);
+        Serial.print("RSY: ");
+        Serial.println(RSYinput);
+      }
+      if (REPORT_triggers) {
+        Serial.print("L2: ");
+        Serial.println(L2input);
+        Serial.print("R2: ");
+        Serial.println(R2input);
+      }
+      if (REPORT_ESC) {
+        Serial.print("ESC_output: ");
+        Serial.println(ESC_output);
+      }
+      if (DEBUG_Servo) {
+        Serial.print("Servo_x_L: ");
+        Serial.println(Servo_x_L_output);
+        Serial.print("Servo_y_L: ");
+        Serial.println(Servo_y_L_output);
+        Serial.print("Servo_x_R: ");
+        Serial.println(Servo_x_R_output);
+        Serial.print("Servo_y_R: ");
+        Serial.println(Servo_y_R_output);
+      }
+      Serial.println("---------------------");
+      Serial.printf("Battery Level: %d", PS4.Battery());
+      if (PS4.Charging()) Serial.println(" (Charging)");
+      else Serial.println();
+    }
+    else Serial.println("Connecting...");
   }
 }
 
@@ -494,6 +487,7 @@ void setup() {
 
   ESC_L.attach(ESC_L_PIN, ESC_min, ESC_max);
   ESC_R.attach(ESC_R_PIN, ESC_min, ESC_max);
+  //L3_R3_Button.setPushDebounceInterval(5000);
 
   servo_x_L.attach(Servo_x_L_PIN, Servo_min, Servo_max);
   servo_y_L.attach(Servo_y_L_PIN, Servo_min, Servo_max);
@@ -503,7 +497,10 @@ void setup() {
 
 
   Serial.begin(115200);
-  PS4.begin();
+  PS4.begin("c0:38:96:b3:c5:c5");
+
+
+
   //Serial.print("WiFi MAC Address:  ");
   //Serial.println(WiFi.macAddress());
   
@@ -518,11 +515,28 @@ void setup() {
 ########################################################################*/
 void loop() {
   if (PS4.isConnected()) {
-    PS4_getControlInput();
-    //setOffset();
-    setSpeed();
+    //Emergency Brake
+    if (PS4.Circle()) {
+      ESC_output = ESC_min;
+      ESC_L.write(ESC_output);
+      ESC_R.write(ESC_output);
+    }
+    pollButtons();
+    pollAnalog();
     setVector();
-  } else {
+    switch (ARMED) {
+      case true:
+        //pollButtons();
+        setSpeed();
+        
+        break;
+      case false:
+        ESC_L.write(ESC_min);
+        ESC_R.write(ESC_min);
+        break;
+    } 
+  } 
+  else {
     ESC_L.write(ESC_min);
     ESC_R.write(ESC_min);
 
@@ -531,5 +545,5 @@ void loop() {
     servo_x_R.write(Servo_mid);
     servo_y_R.write(Servo_mid);
   }
-  DEBUGreport();
+  DEBUG_REPORT();
 }
--- Drive Controller/Trim/Trim.ino
+++ Drive Controller/Trim/Trim.ino
@@ -67,6 +67,8 @@ unsigned long time_now = 0;
 int Gimbal_L_Offset[2] = {0, 0};
 int Gimbal_R_Offset[2] = {0, 0};
 
+int Trim_range[2] = {-200, 200};
+
 
 /*####################################################
 ##  ___      _ _     ___          _               ##
@@ -101,96 +103,39 @@ void nextRainbowColor() {
   if (g > 0 && r == 0) { g--; b++; }
   if (b > 0 && g == 0) { r++; b--; }
 }
-/*
-void PS4_testInputs() {
-  if (PS4.Right()) Serial.println("Right Button");
-  if (PS4.Down()) Serial.println("Down Button");
-  if (PS4.Up()) Serial.println("Up Button");
-  if (PS4.Left()) Serial.println("Left Button");
-
-  if (PS4.Square()) Serial.println("Square Button");
-  if (PS4.Cross()) Serial.println("Cross Button");
-  if (PS4.Circle()) Serial.println("Circle Button");
-  if (PS4.Triangle()) Serial.println("Triangle Button");
-
-  if (PS4.UpRight()) Serial.println("Up Right");
-  if (PS4.DownRight()) Serial.println("Down Right");
-  if (PS4.UpLeft()) Serial.println("Up Left");
-  if (PS4.DownLeft()) Serial.println("Down Left");
-
-  if (PS4.L1()) Serial.println("L1 Button");
-  if (PS4.R1()) Serial.println("R1 Button");
-
-  if (PS4.Share()) Serial.println("Share Button");
-  if (PS4.Options()) Serial.println("Options Button");
-  if (PS4.L3()) Serial.println("L3 Button");
-  if (PS4.R3()) Serial.println("R3 Button");
-
-  if (PS4.PSButton()) Serial.println("PS Button");
-  if (PS4.Touchpad()) Serial.println("Touch Pad Button");
-
-  if (PS4.L2()) {
-    Serial.printf("L2 button at %d\n", PS4.L2Value());
-  }
-  if (PS4.R2()) {
-    Serial.printf("R2 button at %d\n", PS4.R2Value());
-  }
-
-  if (PS4.LStickX()) {
-    Serial.printf("Left Stick x at %d\n", PS4.LStickX());
-  }
-  if (PS4.LStickY()) {
-    Serial.printf("Left Stick y at %d\n", PS4.LStickY());
-  }
-  if (PS4.RStickX()) {
-    Serial.printf("Right Stick x at %d\n", PS4.RStickX());
-  }
-  if (PS4.RStickY()) {
-    Serial.printf("Right Stick y at %d\n", PS4.RStickY());
-  }
-
-  if (PS4.Charging()) Serial.println("The controller is charging");
-  if (PS4.Audio()) Serial.println("The controller has headphones attached");
-  if (PS4.Mic()) Serial.println("The controller has a mic attached");
-
-  Serial.printf("Battery Level : %d\n", PS4.Battery());
-  Serial.println();
-  delay(1000);
-}
-*/
 
 static void Trim_buttonHandler(uint8_t btnId, uint8_t btnState) {
   if (btnState == BTN_PRESSED) {
     switch(btnId) {
       case 0:
-        if (PS4.L1()) {
+        if (PS4.L1() && Gimbal_L_Offset[0] < Trim_range[1]) {
           Gimbal_L_Offset[0] += 1;
         }
-        if (PS4.R1()) {
+        if (PS4.R1() && Gimbal_R_Offset[0] < Trim_range[1]) {
           Gimbal_R_Offset[0] += 1;
         }
         break;
       case 1:
-        if (PS4.L1()) {
+        if (PS4.L1() && Gimbal_L_Offset[0] > Trim_range[0]) {
           Gimbal_L_Offset[0] -= 1;
         }
-        if (PS4.R1()) {
+        if (PS4.R1() && Gimbal_R_Offset[0] > Trim_range[0]) {
           Gimbal_R_Offset[0] -= 1;
         }
         break;
       case 2:
-        if (PS4.L1()) {
+        if (PS4.L1() && Gimbal_L_Offset[1] > Trim_range[0]) {
           Gimbal_L_Offset[1] -= 1;
         }
-        if (PS4.R1()) {
+        if (PS4.R1() && Gimbal_R_Offset[1] > Trim_range[0]) {
           Gimbal_R_Offset[1] -= 1;
         }
         break;
       case 3:
-        if (PS4.L1()) {
+        if (PS4.L1() && Gimbal_L_Offset[1] < Trim_range[1]) {
           Gimbal_L_Offset[1] += 1;
         }
-        if (PS4.R1()) {
+        if (PS4.R1() && Gimbal_R_Offset[1] < Trim_range[1]) {
           Gimbal_R_Offset[1] += 1;
         }
         break;
